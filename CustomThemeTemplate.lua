759daf043601c2dec2a4923ad03535e8f837ff16
LocalSmail <96918131+LocalSmail@users.noreply.github.com>
2022-10-07 19:30:39 +0100

    Create CustomThemeTemplate.lua

---
+++ CustomThemeTemplate.lua
@@ -0,0 +1,8 @@
+Custom_Finity_Theme = {}
+
+Custom_Finity_Theme.CustomThemes = {
+	["Example"] = {} -- add your custom theme stuff inside here
+	-- To make multiple just add a comma and do the samething below
+}
+
+return Custom_Finity_Theme
